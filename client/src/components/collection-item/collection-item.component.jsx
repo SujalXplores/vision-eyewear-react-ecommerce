55d1195692e211c4cd0b60d469171776cffa761b
SujalShah3234 <shahc9437@gmail.com>
2022-01-27 22:29:07 +0530

    🐛 Toaster add on add to cart but still buggy

--- client/src/components/collection-item/collection-item.component.jsx
+++ client/src/components/collection-item/collection-item.component.jsx
@@ -1,5 +1,6 @@
 import { connect } from 'react-redux';
-
+import toast, { Toaster } from 'react-hot-toast';
+import { memo } from 'react';
 import { addItem } from '../../redux/cart/cart.actions';
 import CustomButton from '../custom-button/custom-button.component';
 import './collection-item.styles.css';
@@ -7,29 +8,42 @@ import './collection-item.styles.css';
 export const CollectionItem = ({ item, addItem }) => {
   const { name, price, imageUrl } = item;
 
+  const addToCart = () => {
+    addItem(item);
+    console.warn('first');
+    toast.success('Item added to cart', {
+      id: 'cart-toast',
+      autoClose: 2000,
+      position: 'bottom-center',
+    });
+  };
+
   return (
-    <div className='collection-item-container'>
-      <div
-        className='image background-image'
-        style={{ backgroundImage: `url(${imageUrl})` }}
-      />
-      <div className='collection-footer-container'>
-        <span className='name-container'>{name}</span>
-        <span className='price-container'>
-          {new Intl.NumberFormat('en-IN', {
-            currency: 'INR',
-            style: 'currency',
-            maximumFractionDigits: 0,
-          }).format(price)}
-        </span>
+    <>
+      <Toaster />
+      <div className='collection-item-container'>
+        <div
+          className='image background-image'
+          style={{ backgroundImage: `url(${imageUrl})` }}
+        />
+        <div className='collection-footer-container'>
+          <span className='name-container'>{name}</span>
+          <span className='price-container'>
+            {new Intl.NumberFormat('en-IN', {
+              currency: 'INR',
+              style: 'currency',
+              maximumFractionDigits: 0,
+            }).format(price)}
+          </span>
+        </div>
+        <CustomButton
+          className='btn-add btn-inverted'
+          onClick={() => addToCart()}
+        >
+          Add to cart
+        </CustomButton>
       </div>
-      <CustomButton
-        className='btn-add btn-inverted'
-        onClick={() => addItem(item)}
-      >
-        Add to cart
-      </CustomButton>
-    </div>
+    </>
   );
 };
 
@@ -37,4 +51,4 @@ const mapDispatchToProps = (dispatch) => ({
   addItem: (item) => dispatch(addItem(item)),
 });
 
-export default connect(null, mapDispatchToProps)(CollectionItem);
+export default connect(null, mapDispatchToProps)(memo(CollectionItem));
