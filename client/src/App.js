ce1acf7f66e1acd98f6a2381eefb8741a9e4e897
SujalShah3234 <shahc9437@gmail.com>
2022-01-27 22:27:15 +0530

    🚚 banner moved to homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
-import { useEffect, lazy, Suspense } from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
+import { useEffect, lazy, Suspense } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
-import Header from "./components/header/header.component";
-import Banner from "./components/banner/banner.component";
-import Spinner from "./components/spinner/spinner.component";
-import ErrorBoundary from "./components/error-boundary/error-boundary.component";
+import Header from './components/header/header.component';
+import Spinner from './components/spinner/spinner.component';
+import ErrorBoundary from './components/error-boundary/error-boundary.component';
 
-import { selectCurrentUser } from "./redux/user/user.selectors";
-import { checkUserSession } from "./redux/user/user.actions";
-import Footer from "./components/footer/footer.component";
+import { selectCurrentUser } from './redux/user/user.selectors';
+import { checkUserSession } from './redux/user/user.actions';
+import Footer from './components/footer/footer.component';
 
-const HomePage = lazy(() => import("./pages/homepage/homepage.component"));
-const ShopPage = lazy(() => import("./pages/shop/shop.component"));
+const HomePage = lazy(() => import('./pages/homepage/homepage.component'));
+const ShopPage = lazy(() => import('./pages/shop/shop.component'));
 const SignInAndSignUpPage = lazy(() =>
-  import("./pages/sign-in-and-sign-up/sign-in-and-sign-up.component")
+  import('./pages/sign-in-and-sign-up/sign-in-and-sign-up.component')
 );
-const CheckoutPage = lazy(() => import("./pages/checkout/checkout.component"));
+const CheckoutPage = lazy(() => import('./pages/checkout/checkout.component'));
 
 const App = ({ checkUserSession, currentUser }) => {
   useEffect(() => {
@@ -27,7 +26,6 @@ const App = ({ checkUserSession, currentUser }) => {
   return (
     <>
       <Header />
-      <Banner />
       <Switch>
         <ErrorBoundary>
           <Suspense fallback={<Spinner />}>
--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -1,8 +1,10 @@
+import Banner from '../../components/banner/banner.component';
 import Directory from '../../components/directory/directory.component';
 import './homepage.styles.css';
 
 const HomePage = () => (
   <div className='homepage-container'>
+    <Banner />
     <Directory />
   </div>
 );
