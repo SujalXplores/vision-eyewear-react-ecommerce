98a25426900b207499a8875b5b5e37890f1de2a4
SujalShah3234 <shahc9437@gmail.com>
2022-02-17 23:57:58 +0530

    💄 [update]: signin & signup side picture

--- client/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ client/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -11,7 +11,8 @@ const SignInAndSignUpPage = () => {
         sm={4}
         md={7}
         sx={{
-          backgroundImage: 'url(https://source.unsplash.com/random)',
+          backgroundImage:
+            'url(https://images.unsplash.com/photo-1577744486770-020ab432da65?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80)',
           backgroundRepeat: 'no-repeat',
           backgroundSize: 'cover',
           backgroundPosition: 'center',
