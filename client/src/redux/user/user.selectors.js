ab0e4899ae67b1c498355b771d9f78ffbf0d9811
SujalShah3234 <shahc9437@gmail.com>
2022-03-01 17:13:13 +0530

    🥅 Signup form validation

--- client/src/redux/user/user.selectors.js
+++ client/src/redux/user/user.selectors.js
@@ -1,8 +1,13 @@
 import { createSelector } from 'reselect';
 
-const selectUser = state => state.user;
+const selectUser = (state) => state.user;
 
 export const selectCurrentUser = createSelector(
   [selectUser],
-  user => user.currentUser
+  (user) => user.currentUser
+);
+
+export const selectErrorMessage = createSelector(
+  [selectUser],
+  (user) => user.error
 );
