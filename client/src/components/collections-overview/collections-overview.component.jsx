739b487d6cea5a0ca5730233cf75811240427359
Vishal Rathod <rvd31305@gmail.com>
2022-03-05 16:26:51 +0530

    Refactor: CSS replced with CSS Modules

--- client/src/components/collections-overview/collections-overview.component.jsx
+++ client/src/components/collections-overview/collections-overview.component.jsx
@@ -1,12 +1,12 @@
-import { useSelector } from 'react-redux';
-import CollectionPreview from '../collection-preview/collection-preview.component';
-import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
-import './collections-overview.styles.css';
+import { useSelector } from "react-redux";
+import CollectionPreview from "../collection-preview/collection-preview.component";
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
+import styles from "./collections-overview.module.css";
 
 export const CollectionsOverview = () => {
   const collections = useSelector(selectCollectionsForPreview);
   return (
-    <div className='collections-overview-container'>
+    <div className={styles["collections-overview-container"]}>
       {collections.map(({ id, ...otherCollectionProps }) => (
         <CollectionPreview key={id} {...otherCollectionProps} />
       ))}
