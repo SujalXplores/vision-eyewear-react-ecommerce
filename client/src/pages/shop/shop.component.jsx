2178290a5d145572b78f0ee753912c1aa13f1575
Vishal Rathod <rvd31305@gmail.com>
2022-03-05 16:38:11 +0530

    Refactor: CSS replced with CSS Modules

--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -1,8 +1,8 @@
-import { Outlet } from 'react-router-dom';
-import './shop.styles.css';
+import { Outlet } from "react-router-dom";
+import styles from "./shop.module.css";
 
 export const ShopPage = () => (
-  <div className='shop-page-container'>
+  <div className={styles["shop-page-container"]}>
     <Outlet />
   </div>
 );
