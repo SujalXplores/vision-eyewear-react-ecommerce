5b74c524c5488d94a6589ef69dc392511cd33865
Vishal Rathod <rvd31305@gmail.com>
2022-03-05 16:36:40 +0530

    Refactor: CSS replced with CSS Modules

--- client/src/pages/collection/collection.component.jsx
+++ client/src/pages/collection/collection.component.jsx
@@ -1,18 +1,18 @@
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 
-import CollectionItem from '../../components/collection-item/collection-item.component';
-import { selectCollection } from '../../redux/shop/shop.selectors';
-import './collection.styles.css';
+import CollectionItem from "../../components/collection-item/collection-item.component";
+import { selectCollection } from "../../redux/shop/shop.selectors";
+import styles from "./collection.module.css";
 
 export const CollectionPage = () => {
   const collection = useSelector(
-    selectCollection(window.location.pathname.split('/').pop())
+    selectCollection(window.location.pathname.split("/").pop())
   );
 
   return (
-    <div className='collection-page-container'>
-      <h2 className='collection-title'>{collection.title}</h2>
-      <div className='collection-items-container'>
+    <div className={styles["collection-page-container"]}>
+      <h2 className={styles["collection-title"]}>{collection.title}</h2>
+      <div className={styles["collection-items-container"]}>
         {collection.items.map((item) => (
           <CollectionItem key={item.id} item={item} />
         ))}
