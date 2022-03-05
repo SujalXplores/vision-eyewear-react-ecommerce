73722123d5ef086eaf4af58e6d18bdb3bfef3683
Vishal Rathod <rvd31305@gmail.com>
2022-03-05 16:26:09 +0530

    Refactor: CSS replced with CSS Modules

--- client/src/components/collection-preview/collection-preview.component.jsx
+++ client/src/components/collection-preview/collection-preview.component.jsx
@@ -1,21 +1,21 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
-import CollectionItem from '../collection-item/collection-item.component';
-import './collection-preview.styles.css';
+import CollectionItem from "../collection-item/collection-item.component";
+import styles from "./collection-preview.module.css";
 
 export const CollectionPreview = ({ title, items, routeName }) => {
   const navigate = useNavigate();
-  
+
   const handleClick = () => {
     navigate(`/shop/${routeName}`);
   };
 
   return (
-    <div className='collection-preview-container'>
-      <h1 className='collection-title' onClick={handleClick}>
+    <div className={styles["collection-preview-container"]}>
+      <h1 className={styles["collection-title"]} onClick={handleClick}>
         {title.toUpperCase()}
       </h1>
-      <div className='preview-container'>
+      <div className={styles["preview-container"]}>
         {items
           .filter((item, idx) => idx < 4)
           .map((item) => (
