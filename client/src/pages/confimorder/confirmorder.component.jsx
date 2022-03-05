99198dee75c878b7807f3c904432c69929a2fed4
Vishal Rathod <rvd31305@gmail.com>
2022-03-05 16:37:01 +0530

    Refactor: CSS replced with CSS Modules

--- client/src/pages/confimorder/confirmorder.component.jsx
+++ client/src/pages/confimorder/confirmorder.component.jsx
@@ -1,14 +1,20 @@
-import './confirmorder.styles.css';
+import styles from "./confirmorder.module.css";
 
 const GreenCheck = () => (
   <svg
-    className='checkmark'
+    className={styles["checkmark"]}
     xmlns='http://www.w3.org/2000/svg'
     viewBox='0 0 52 52'
   >
-    <circle className='checkmark__circle' cx='26' cy='26' r='25' fill='none' />
+    <circle
+      className={styles["checkmark__circle"]}
+      cx='26'
+      cy='26'
+      r='25'
+      fill='none'
+    />
     <path
-      className='checkmark__check'
+      className={styles["checkmark__check"]}
       fill='none'
       d='M14.1 27.2l7.1 7.2 16.7-16.8'
     />
@@ -19,8 +25,8 @@ export default function ConfirmOrder() {
   return (
     <>
       <GreenCheck />
-      <div className='order-complete-text'>
-        <h2 className='order-complete-h2'>Your order is complete!</h2>
+      <div className={styles["order-complete-text"]}>
+        <h2 className={styles["order-complete-h2"]}>Your order is complete!</h2>
         <p>You will be receiving a confirmation email with order details.</p>
       </div>
     </>
