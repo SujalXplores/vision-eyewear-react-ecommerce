56c4a5cd66a82ede85dab992bd666bf35843a78a
Vishal Rathod <rvd31305@gmail.com>
2022-03-05 16:37:19 +0530

    Refactor: CSS replced with CSS Modules

--- client/src/pages/error404/pagenotfound.component.jsx
+++ client/src/pages/error404/pagenotfound.component.jsx
@@ -1,17 +1,17 @@
-import { Button } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
-import './pagenotfound.styles.css';
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import styles from "./pagenotfound.module.css";
 
 const PageNotFound = () => {
   const navigate = useNavigate();
 
   const goToHome = () => {
-    navigate('/');
+    navigate("/");
   };
 
   return (
-    <div className='page-not-found'>
-      <div className='text-404'>
+    <div className={styles["page-not-found"]}>
+      <div className={styles["text-404"]}>
         <h1>Page not found</h1>
         <p>The page you are looking for does not exist.</p>
       </div>
