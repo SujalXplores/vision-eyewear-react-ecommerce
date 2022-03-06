408799fce3507445cfa436ea796027336fd51df5
Vishal Rathod <rvd31305@gmail.com>
2022-03-06 18:49:47 +0530

    Refactor: Inline styles moved in external CSS

--- client/src/components/spinner/spinner.component.jsx
+++ client/src/components/spinner/spinner.component.jsx
@@ -1,10 +1,13 @@
-import Backdrop from '@mui/material/Backdrop';
-import CircularProgress from '@mui/material/CircularProgress';
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
+
+import styles from "./spinner.module.css";
 
 const Spinner = () => (
   <>
     <Backdrop
-      sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      className={styles.backdrop}
+      sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
       open={true}
     >
       <CircularProgress color='inherit' />
