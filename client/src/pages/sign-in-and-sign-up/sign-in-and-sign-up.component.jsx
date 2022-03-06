3fa6c075cd7505cd78e6ea4c9fc137fca007efc4
Vishal Rathod <rvd31305@gmail.com>
2022-03-06 18:50:35 +0530

    Refactor: Inline styles moved in external CSS

--- client/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ client/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,22 +1,18 @@
-import { CssBaseline, Grid } from '@mui/material';
-import { Outlet } from 'react-router-dom';
+import { CssBaseline, Grid } from "@mui/material";
+import { Outlet } from "react-router-dom";
+
+import styles from "./sign-in-and-sign-up.module.css";
 
 const SignInAndSignUpPage = () => {
   return (
-    <Grid container component='main' sx={{ height: '100vh' }}>
+    <Grid container component='main' className={styles.root}>
       <CssBaseline />
       <Grid
         item
         xs={false}
         sm={4}
         md={7}
-        sx={{
-          backgroundImage:
-            'url(https://images.unsplash.com/photo-1577744486770-020ab432da65?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80)',
-          backgroundRepeat: 'no-repeat',
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-        }}
+        className={styles["sign-in-and-sign-up-image-container"]}
       />
       <Outlet />
     </Grid>
