18076f39d45210ef3b0fcd3ba627535d665bc18f
SujalShah3234 <shahc9437@gmail.com>
2022-03-11 22:39:44 +0530

    🐛 product details routing bug fix

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,30 +1,31 @@
-import { lazy } from "react";
-import { useSelector } from "react-redux";
-import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { lazy } from 'react';
+import { useSelector } from 'react-redux';
+import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
 
-import { selectCurrentUser } from "./redux/user/user.selectors";
-import SignIn from "./components/sign-in/sign-in.component";
-import SignUp from "./components/sign-up/sign-up.component";
+import { selectCurrentUser } from './redux/user/user.selectors';
+import SignIn from './components/sign-in/sign-in.component';
+import SignUp from './components/sign-up/sign-up.component';
+import ProductDetails from './pages/product-details/product-details';
 
-const HomePage = lazy(() => import("./pages/homepage/homepage.component"));
-const Profile = lazy(() => import("./pages/profile/profile.component"));
-const ShopPage = lazy(() => import("./pages/shop/shop.component"));
-const CheckoutPage = lazy(() => import("./pages/checkout/checkout.component"));
-const ContactUs = lazy(() => import("./pages/contactus/contactus.component"));
+const HomePage = lazy(() => import('./pages/homepage/homepage.component'));
+const Profile = lazy(() => import('./pages/profile/profile.component'));
+const ShopPage = lazy(() => import('./pages/shop/shop.component'));
+const CheckoutPage = lazy(() => import('./pages/checkout/checkout.component'));
+const ContactUs = lazy(() => import('./pages/contactus/contactus.component'));
 const ConfirmOrder = lazy(() =>
-  import("./pages/confimorder/confirmorder.component")
+  import('./pages/confimorder/confirmorder.component')
 );
 const PageNotFound = lazy(() =>
-  import("./pages/error404/pagenotfound.component")
+  import('./pages/error404/pagenotfound.component')
 );
 const SignInAndSignUpPage = lazy(() =>
-  import("./pages/sign-in-and-sign-up/sign-in-and-sign-up.component")
+  import('./pages/sign-in-and-sign-up/sign-in-and-sign-up.component')
 );
 const CollectionsOverviewContainer = lazy(() =>
-  import("./components/collections-overview/collections-overview.container")
+  import('./components/collections-overview/collections-overview.container')
 );
 const CollectionPage = lazy(() =>
-  import("./pages/collection/collection.component")
+  import('./pages/collection/collection.component')
 );
 
 const Router = () => {
@@ -47,31 +48,24 @@ const Router = () => {
 
   return (
     <Routes>
-      <Route index path='/' element={<HomePage />} />
-      <Route path='contactus' element={<ContactUs />} />
-      <Route path='shop/*' element={<ShopPage />}>
-        <Route path='' index element={<CollectionsOverviewContainer />} />
-        <Route path=':collectionId' element={<CollectionPage />} />
+      <Route path='/' element={<HomePage />} />
+      <Route path='/contactus' element={<ContactUs />} />
+      <Route path='/shop' element={<ShopPage />}>
+        <Route index element={<CollectionsOverviewContainer />} />
+        <Route path='category/:collectionId' element={<CollectionPage />} />
+        <Route
+          path='category/:collectionId/:productId'
+          element={<ProductDetails />}
+        />
+        <Route path=':productId' element={<ProductDetails />} />
       </Route>
-      <Route path='order-confirmed' element={<ConfirmOrder />} />
+      <Route element={<ProtectedRoute />}>
+        <Route path='/checkout' element={<CheckoutPage />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/order-confirmed' element={<ConfirmOrder />} />
+      </Route>
       <Route
-        path='checkout'
-        element={
-          <ProtectedRoute>
-            <CheckoutPage />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path='profile'
-        element={
-          <ProtectedRoute>
-            <Profile />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path='auth'
+        path='/auth'
         element={
           <HideLogin>
             <SignInAndSignUpPage />
--- client/src/components/collection-preview/collection-preview.component.jsx
+++ client/src/components/collection-preview/collection-preview.component.jsx
@@ -1,21 +1,21 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
-import CollectionItem from "../collection-item/collection-item.component";
-import styles from "./collection-preview.module.css";
+import CollectionItem from '../collection-item/collection-item.component';
+import styles from './collection-preview.module.css';
 
 export const CollectionPreview = ({ title, items, routeName }) => {
   const navigate = useNavigate();
 
   const handleClick = () => {
-    navigate(`/shop/${routeName}`);
+    navigate(`/shop/category/${routeName}`);
   };
 
   return (
-    <div className={styles["collection-preview-container"]}>
-      <h1 className={styles["collection-title"]} onClick={handleClick}>
+    <div className={styles['collection-preview-container']}>
+      <h1 className={styles['collection-title']} onClick={handleClick}>
         {title.toUpperCase()}
       </h1>
-      <div className={styles["preview-container"]}>
+      <div className={styles['preview-container']}>
         {items
           .filter((item, idx) => idx < 4)
           .map((item) => (
