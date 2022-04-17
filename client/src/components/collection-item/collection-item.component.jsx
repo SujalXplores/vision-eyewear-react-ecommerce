4c83d3007c0281de6439a45c52d636f6905000ae
Vishal Rathod <rvd31305@gmail.com>
2022-04-17 13:31:17 +0530

    Styles: Product card styles changed
    - Refactored product card styles

--- client/src/components/collection-item/collection-item.component.jsx
+++ client/src/components/collection-item/collection-item.component.jsx
@@ -1,9 +1,10 @@
 import { useNavigate } from 'react-router-dom';
-import { Card, CardMedia, CardActionArea } from '@mui/material';
+import { Card, CardMedia, CardActionArea, CardContent, Typography } from '@mui/material';
 
 import styles from './collection-item.module.css';
 
 export const CollectionItem = ({ item }) => {
+  console.log(item);
   const navigate = useNavigate();
 
   const handleViewMoreDetails = () => {
@@ -20,6 +21,17 @@ export const CollectionItem = ({ item }) => {
           image={item.imageUrl}
           alt={item.name}
         />
+        <CardContent>
+        <Typography variant="body2" color="#0F1111" fontWeight={700}>
+          {item.name}
+        </Typography>
+        <Typography variant="body2" color="#656666">
+         {item.frame_shape}
+        </Typography>
+        <Typography gutterBottom variant="h6" color="#B12704" component="div" >
+        ₹ {item.price}
+        </Typography>
+      </CardContent>
       </CardActionArea>
     </Card>
   );
